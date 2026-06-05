31cde4da17402cf68258e419f47f4df73625c589
kritiagrawal2207 <kriti7807karauli@gmail.com>
2026-06-05 11:20:11 +0530

    Backendsetup

---
+++ Workforce & Task management System/WorkSyncAPI/Models/Employee.cs
@@ -0,0 +1,9 @@
+namespace WorkSyncAPI.Models;
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Department { get; set; } = string.Empty;
+    public bool IsActive { get; set; } = true;
+}
