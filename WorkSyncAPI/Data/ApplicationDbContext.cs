e0b12dc4bd46f44f6beacac6b8b6075bf8578184
kritiagrawal2207 <kriti7807karauli@gmail.com>
2026-06-14 01:41:06 +0530

    updated week1

---
+++ WorkSyncAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WorkSyncAPI.Models;
+
+namespace WorkSyncAPI.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+}
