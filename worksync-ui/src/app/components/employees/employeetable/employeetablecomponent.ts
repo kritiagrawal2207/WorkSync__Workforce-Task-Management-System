838383fea5d327b600478ff4e30ec04b8b1a3550
kritiagrawal2207 <kriti7807karauli@gmail.com>
2026-07-03 14:57:34 +0530

    changes

--- worksync-ui/src/app/components/employees/employeetable/employeetablecomponent.ts
+++ worksync-ui/src/app/components/employees/employeetable/employeetablecomponent.ts
@@ -1,3 +1,4 @@
+
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Employee } from '../../../models/employeemodel';
 import { EmployeeRowComponent } from '../employeerow/employeerowcomponent';
