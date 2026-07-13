2af606d7099f13eaab21648dd8974d0fcd24c03f
kritiagrawal2207 <kriti7807karauli@gmail.com>
2026-07-13 07:02:28 +0530

    fix loader of login page

--- WorkSyncAPI/obj/Debug/net10.0/WorkSyncAPI.AssemblyInfo.cs
+++ WorkSyncAPI/obj/Debug/net10.0/WorkSyncAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkSyncAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9eeadb356ece60158b97a08da4a02451f4d655d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34e6274e9b29f77be04248959fdddd32148e95b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkSyncAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkSyncAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
